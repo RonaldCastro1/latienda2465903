4515a6b3f23bc482571e198a8f041f12b6c7881b
Your Name <you@example.com>
2022-04-21 17:34:40 -0500

    crear proyecyo y arreglos

---
+++ routes/web.php
@@ -0,0 +1,86 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//primera ruta
+Route::get('Hola',function(){
+    echo "hola 2465903";
+});
+
+//Ruta de arreglos
+Route::get('arreglo', function(){
+    $estudiantes = [ 
+        "CA" => 1, 
+        "JO" => true, 
+        "AN" => 1.78
+    ];
+    echo "<pre>";
+    var_dump($estudiantes);
+    echo "</pre>";
+//recorrer el arreglo
+foreach($estudiantes as $e){
+    echo $e."<hr />";
+};
+//agregar elementos en php
+$estudiantes["RO"]="Ronald";
+    var_dump($estudiantes);
+});
+
+
+Route::get('paises', function(){
+    $paises = [
+        "Colombia" => [
+            "capital" => "Bogota",
+            "moneda" => "Peso colombiano",
+            "poblacion" => 51,
+            "ciudades" => [
+                "medellin",
+                "cali",
+                "barranquilla"
+            ]
+        ],
+        "Peru" => [
+            "capital" => "Lima",
+            "moneda" => "Sol",
+            "poblacion" => 32,
+            "ciudades" => [
+                "arequipa",
+                "trujillo"
+            ]
+        ],
+        "Paraguay" => [
+            "capital" => "Asuncion",
+            "moneda" => "Guarani",
+            "poblacion" => 7,
+            "ciudades" => [
+                "luque"
+        ],
+        "Brazil" => [
+            "capital" => "Brasilia",
+            "moneda" => "Real brasileño",
+            "poblacion" => 212
+        ],
+        "Mexico" => [
+            "capital" => "Ciudad de mexico",
+            "moneda" => "Peso mexicano",
+            "poblacion" => 128
+        ]
+      ]
+    ];
+    return view ('paises')-> with("paises", $paises);
+    
+});
